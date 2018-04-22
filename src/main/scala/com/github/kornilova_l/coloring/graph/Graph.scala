1672f4b72ac2e2835b11eda1a0c0d8475d1ca354
kornilova-l <kornilova203@gmail.com>
2018-04-22 21:28:33 +0300

    Make graph immutable

--- src/main/scala/com/github/kornilova_l/coloring/graph/Graph.scala
+++ src/main/scala/com/github/kornilova_l/coloring/graph/Graph.scala
@@ -1,34 +1,11 @@
 package com.github.kornilova_l.coloring.graph
 
-import scala.collection.mutable
 
-class Graph {
-  private val nodes = mutable.Map[Int, Node]()
-
-  def addNode(id: Int) {
-    if (nodes.contains(id)) {
-      throw new IllegalArgumentException(s"This id already exist in node: $id")
-    }
-    nodes.update(id, new Node(id))
-  }
-
-  def addConnection(id1: Int, id2: Int): Unit = {
-    if (!nodes.contains(id1) || !nodes.contains(id2)) {
-      throw new IllegalArgumentException(s"One or both ids do not exist in tree: $id1 $id2")
-    }
-    val node1 = nodes(id1)
-    val node2 = nodes(id2)
-
-    node1.addNeighbour(node2)
-    node2.addNeighbour(node1)
-  }
-
-  private class Node(val id: Int) {
-    private val neighbours = mutable.HashSet[Node]()
-
-    def addNeighbour(neighbour: Node): Unit = {
-      neighbours add neighbour
-    }
-  }
-
+class Graph(nodeIds: Set[Int], connections: Map[Int, Set[Int]]) {
+  val nodes: Map[Int, Node] = (
+    for (nodeId <- nodeIds)
+      yield nodeId -> new Node(nodeId, connections.getOrElse(nodeId, Set[Int]()))
+    ) toMap
 }
+
+class Node(val id: Int, val neighbours: Set[Int])
--- src/main/scala/com/github/kornilova_l/coloring/graph/GraphGenerator.scala
+++ src/main/scala/com/github/kornilova_l/coloring/graph/GraphGenerator.scala
@@ -4,32 +4,32 @@ import scala.collection.mutable
 import scala.util.Random
 
 class GraphGenerator(size: Int, maximumDegree: Int, density: Float = 0.8f) {
-  val graph = new Graph()
+  val graph: Graph = {
+    val nodeIds = (for (i <- 1 to size) yield i) toSet
 
-  {
-    for (i <- 1 to size) {
-      graph.addNode(i)
-    }
-
-    val connectionsCount = mutable.Map[Int, Int]()
+    val connections = mutable.Map[Int, mutable.Set[Int]]().withDefault(_ => mutable.Set[Int]())
 
     val random = new Random()
 
-    for (i <- 0 to Math.floor(size * maximumDegree * density).toInt) {
+    for (_ <- 0 to Math.floor(size * maximumDegree * density).toInt) {
       val id1 = random.nextInt(size - 1) + 1
       val id2 = random.nextInt(size - 1) + 1
 
-      val node1ConnectionsCount = connectionsCount.getOrElse(id1, 0)
-      val node2ConnectionsCount = connectionsCount.getOrElse(id2, 0)
+      val neighbours1 = connections(id1)
+      val neighbours2 = connections(id2)
 
       if (id1 != id2 &&
-        node1ConnectionsCount < maximumDegree &&
-        node2ConnectionsCount < maximumDegree) {
+        neighbours1.size < maximumDegree &&
+        neighbours2.size < maximumDegree) {
 
-        graph.addConnection(id1, id2)
-        connectionsCount.update(id1, node1ConnectionsCount + 1)
-        connectionsCount.update(id2, node2ConnectionsCount + 1)
+        neighbours1 add id2
+        neighbours2 add id1
+
+        connections += (id1 -> neighbours1)
+        connections += (id2 -> neighbours2)
+
       }
     }
+    new Graph(nodeIds, (for ((key, value) <- connections) yield key -> value.toSet) toMap)
   }
 }
